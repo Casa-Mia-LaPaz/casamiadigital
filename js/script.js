7bef5935809e92bec8ce43f2bb5bcc0db765f298
casa-mia-lapaz <acsarsrlcasamia@gmail.com>
2026-07-28 14:59:18 -0400

    Version 0.3 - Estructura e imágenes del menú

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("pizzas").addEventListener("click", function () {
+    window.location.href = "paginas/pizzas.html";
+});
+
+document.getElementById("pastas").addEventListener("click", function () {
+    alert("Entrando al menú de Pastas");
+});
+
+document.getElementById("lasagna").addEventListener("click", function () {
+    alert("Entrando al menú de Lasaña");
+});
+
+document.getElementById("bebidas").addEventListener("click", function () {
+    alert("Entrando al menú de Bebidas");
+});
+
+document.getElementById("promociones").addEventListener("click", function () {
+    alert("Entrando al menú de Promociones");
+});
