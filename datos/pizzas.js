c7704e0323f81a568e42c6eaf322df32c9ce40a0
casa-mia-lapaz <acsarsrlcasamia@gmail.com>
2026-07-29 14:10:23 -0400

    Versión 0.3 - Estructura de datos de pizzas

---
+++ datos/pizzas.js
@@ -0,0 +1,82 @@
+const pizzas = [
+    {
+        nombre: "Americana",
+        descripcion: "Jamón, mozzarella y salsa de tomate.",
+        imagen: "../imagenes/pizzas/americana.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Carnívora",
+        descripcion: "Jamón, salame, chorizo y mozzarella.",
+        imagen: "../imagenes/pizzas/carnivora.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Hawaiana",
+        descripcion: "Jamón, piña y mozzarella.",
+        imagen: "../imagenes/pizzas/hawaiana.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Vegetariana",
+        descripcion: "Vegetales frescos y mozzarella.",
+        imagen: "../imagenes/pizzas/vegetariana.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Salame",
+        descripcion: "Salame italiano y mozzarella.",
+        imagen: "../imagenes/pizzas/salame.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Funghi e Prosciutto",
+        descripcion: "Champiñones, jamón y mozzarella.",
+        imagen: "../imagenes/pizzas/funghi.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Cuatro Quesos",
+        descripcion: "Mezcla especial de cuatro quesos.",
+        imagen: "../imagenes/pizzas/cuatroquesos.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    },
+    {
+        nombre: "Brisa Toscana",
+        descripcion: "Especialidad de Casa Mía.",
+        imagen: "../imagenes/pizzas/brisatoscana.jpg",
+        precios: {
+            cuatro: 47,
+            seis: 69,
+            ocho: 91
+        }
+    }
+];
